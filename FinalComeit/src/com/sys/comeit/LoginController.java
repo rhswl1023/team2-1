e721ee55f6fb4342dc2a5c02a5cc121bfd6f4399
rhswl1023 <rhswl1023@naver.com>
2020-07-21 00:46:56 +0900

    2020-07-21 12:46 hyeji memberlogin

--- FinalComeit/src/com/sys/comeit/LoginController.java
+++ FinalComeit/src/com/sys/comeit/LoginController.java
@@ -1,6 +1,27 @@
 package com.sys.comeit;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class LoginController
 {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value = "/memberlogin.action", method = RequestMethod.GET)
+	public String memberJoin(Model model)
+	{
+		String view = null;
+		
+		view = "/WEB-INF/views/member/MemberLogin.jsp";
+		
+		return view;
+	}
+	
 
 }
