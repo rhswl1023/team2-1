b0e6aa3e28e30bab0f81483de15fd139f301b8e7
KiHyeChang <gihyej@gmail.com>
2020-07-21 18:15:39 +0900

    2020-07-21 18:15 kihye login/admin,member,spa DAO, memberDAO.xml

--- FinalComeit/src/com/sys/comeit/IMemberDAO.java
+++ FinalComeit/src/com/sys/comeit/IMemberDAO.java
@@ -8,11 +8,14 @@ import java.util.ArrayList;
 
 public interface IMemberDAO
 {
-	public int add(MemberDTO dto);	// 실제 회원 가입 insert
-	public int count();				// 회원 수
-	public ArrayList<MemberDTO> list();	// 회원 리스트	
-	public int modify(MemberDTO dto);	// 회원이 자기 정보 수정 update
-	public int remove(int mid); 		// 회원탈퇴(프로시저로 탈퇴될텐데?)
-	public MemberDTO login(MemberDTO dto); //로그인
+	public int memberAdd(MemberDTO dto);	// 실제 회원 가입 insert
+	public int memberCount();			// 회원 수
+	public ArrayList<MemberDTO> memberList();	// 회원 리스트	
+	public int memberModify(MemberDTO dto);	// 회원이 자기 정보 수정 update
+	public int memberRemove(int mid); 		// 회원탈퇴(프로시저로 탈퇴될텐데?)
+	
+	public String memberLogin(MemberDTO dto); //로그인
+	public String memStop(String id);		// 정지 여부 확인
+	public int memBlock(String id);			// 블락 여부 확인
 
 }
--- FinalComeit/src/com/sys/comeit/LoginController.java
+++ FinalComeit/src/com/sys/comeit/LoginController.java
@@ -1,4 +1,3 @@
-/*
 package com.sys.comeit;
 
 import javax.servlet.http.HttpServletRequest;
@@ -43,38 +42,40 @@ public class LoginController
       String pwd = request.getParameter("formPassword");
       String loginType = request.getParameter("loginType");
       String name = null;
-      int stopCount = 0;
+      String stopCode = null;
+      int blockCount = 0;
       
       
       // 로그인 정보 확인 및 정지 여부 조회
       //! loginType = "0" 일반 회원 로그인, "1" 업체 회원 로그인 , "2" 관리자 로그인
       if (loginType.equals("0")) 
       {
-         MemberDTO dto = new MemberDTO();
+    	  MemberDTO dto = new MemberDTO();
 
           dto.setId(id);
           dto.setPwd(pwd);  
           
           name = memDao.memberLogin(dto);
-          stopCount = memDao.stopSearch(id);
+          stopCode = memDao.memStop(id);
+          blockCount = memDao.memBlock(id);
       }
       else if(loginType.equals("1")) 
       {
-         SpaDTO dto = new SpaDTO();
-         
-         dto.setSpa_id(id);
-         dto.setPwd(pwd);
-         
-         name = spaDao.spaLogin(dto);
+    	  SpaDTO dto = new SpaDTO();
+    	  
+    	  dto.setSpa_id(id);
+    	  dto.setPwd(pwd);
+    	  
+    	  name = spaDao.spaLogin(dto);
       }
       else if(loginType.equals("2"))
       {
-         AdminDTO dto = new AdminDTO();
-         
-         dto.setAdmin_cd(id);
-         dto.setPwd(pwd);
-         
-         name = admDao.adminLogin(dto);
+    	  AdminDTO dto = new AdminDTO();
+    	  
+    	  dto.setAdmin_cd(id);
+    	  dto.setPwd(pwd);
+    	  
+    	  name = admDao.adminLogin(dto);
       }
       
      
@@ -84,32 +85,39 @@ public class LoginController
          request.setAttribute("msg", "아이디와 패스워드가 일치하지 않습니다.");
          result = "redirect:memberlogin.action";
       }
-      else if(stopCount != 0)      // 정지된 내역이 있을시
+      else if(blockCount != 0) // 블락된 내역이 있을시
       {
-         
-         result = "/WEB-INF/views/header.jsp";
+    	  
+    	  result = "/WEB-INF/views/header.jsp";
       }
-      else          // 로그인 성공시
+      else if(stopCode != null)		// 정지된 내역이 있을시
+      {
+    	      	  
+    	  result = "/WEB-INF/views/header.jsp";
+      }
+      else 			// 로그인 성공시
       {
          HttpSession session = request.getSession();
          
          session.setAttribute("name", name);
          session.setAttribute("id", id);
-         session.setAttribute("pwd", pwd);     
+         session.setAttribute("pwd", pwd); 	 
          session.setAttribute("loginType", loginType);
          
-         if (loginType.equals("2"))                   // 관리자 로그인
-            result = "";
-         else                                     // 업체 및 회원 로그인
-            result = "/WEB-INF/views/MainPage.jsp";
+         if (loginType.equals("2")) 						// 관리자 로그인
+        	 result = "";
+         else 												// 업체 및 회원 로그인
+        	 result = "/WEB-INF/views/MainPage.jsp";
       }
       
       // 테스트
-      //System.out.println(name+id+pwd+loginType); 
+		/* System.out.println(name+id+pwd+loginType); */
       
       return result;
       
    }
+   
+   
    // 아이디 찾기
    @RequestMapping(value = "/searchId.action", method = RequestMethod.GET)
    public String searchId(HttpServletRequest request)
@@ -141,4 +149,3 @@ public class LoginController
 	   return view;
    }
 }
-*/
