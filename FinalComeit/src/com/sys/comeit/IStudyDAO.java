b0998a79b74a8abeadba3c6d3e45ae7ea8561baa
Song-Sujin <ssj1744@naver.com>
2020-07-23 15:01:14 +0900

    2020-07-23 15:00 sujin IStudyDAO

--- FinalComeit/src/com/sys/comeit/IStudyDAO.java
+++ FinalComeit/src/com/sys/comeit/IStudyDAO.java
@@ -1,8 +1,17 @@
 package com.sys.comeit;
 
+import java.util.ArrayList;
+
 public interface IStudyDAO
 {
 	public StudyDTO studyInfoSearch(String stu_cd);			// 스터디방 정보 조회
 	
+	public String studyInsert(StudyDTO dto);		// 스터디 개설 
+	public int studyDayInsert(String day);			// 스터디 진행 요일 insert
+	public int studyIntTagInsert(StudyDTO dto);		// 스터디 관심 키워드 insert
+	public int studyEtcTagInsert(StudyDTO dto);		// 스터디 관심 기타 키워드 insert
+	public int studyEtcTagCount(String etcTag);		// 스터디 관심 기타 키워드 10개 체크
 	
+	public ArrayList<StudyDTO> studyList();			// 스터디 리스트
+	public int studyModify(StudyDTO dto);			// 스터디 정보 수정
 }
--- FinalComeit/src/com/sys/comeit/StudyDTO.java
+++ FinalComeit/src/com/sys/comeit/StudyDTO.java
@@ -6,12 +6,92 @@ public class StudyDTO
 	private String stu_cd, str_date, cmt_date, crt_date; 		// 스터디개설코드, 시작일, 확정일, 게시글등록일
 	private String title, mem_num, content;				 		// 방제목, 인원수, 상세설명
 	private String stu_hist_cd, stu_join_cd, apntm_dat; 		// 스터디장이력코드, (스터디장)참가코드, 임명일
-	//private String leader_mem_cd, leader_id, name;				// 스터디장 회원코드, 스터디장 아이디, 스터디장 이름
+	private String leader_mem_cd, leader_name;					// 스터디장 회원코드, 스터디장 이름
 	private String meet_term, area_name, spc_area_name;			// 기간, 지역명, 세부지역명 
 	private String idntt_type, lv, cat_name, scrt_num;			// 신분유형, 레벨, 카테고리, 방비밀번호(없으면 0 나옴)
 	
+	// 개설하기
+	// 스터디방 제목, 세부키워드, 상세설명, 모집인원, 활동 지역, 레벨, 시작날짜
+	// 종료날짜, 모임 지정 요일, 비밀방 설정 여부
+	// 대표이미지, 스터디신분, 레벨코드, 신분코드
+	
+	// 개설 프로시저 : 개설자, 세부지역, 모임기간코드, 신분코드, 레벨코드, 카테고리코드, 제목, 시작날짜, 인원수, 내용
+	private String crt_mem_cd, spc_area_cd, meet_term_cd, idntt_cd, lv_cd, stu_cat_cd, img_url;
 	
 	// getter / setter 구성
+	public String getLeader_mem_cd()
+	{
+		return leader_mem_cd;
+	}
+	public void setLeader_mem_cd(String leader_mem_cd)
+	{
+		this.leader_mem_cd = leader_mem_cd;
+	}
+	public String getLeader_name()
+	{
+		return leader_name;
+	}
+	public void setLeader_name(String leader_name)
+	{
+		this.leader_name = leader_name;
+	}
+	public String getCrt_mem_cd()
+	{
+		return crt_mem_cd;
+	}
+	public void setCrt_mem_cd(String crt_mem_cd)
+	{
+		this.crt_mem_cd = crt_mem_cd;
+	}
+	public String getSpc_area_cd()
+	{
+		return spc_area_cd;
+	}
+	public void setSpc_area_cd(String spc_area_cd)
+	{
+		this.spc_area_cd = spc_area_cd;
+	}
+	public String getMeet_term_cd()
+	{
+		return meet_term_cd;
+	}
+	public void setMeet_term_cd(String meet_term_cd)
+	{
+		this.meet_term_cd = meet_term_cd;
+	}
+	public String getIdntt_cd()
+	{
+		return idntt_cd;
+	}
+	public void setIdntt_cd(String idntt_cd)
+	{
+		this.idntt_cd = idntt_cd;
+	}
+	public String getLv_cd()
+	{
+		return lv_cd;
+	}
+	public void setLv_cd(String lv_cd)
+	{
+		this.lv_cd = lv_cd;
+	}
+	public String getStu_cat_cd()
+	{
+		return stu_cat_cd;
+	}
+	public void setStu_cat_cd(String stu_cat_cd)
+	{
+		this.stu_cat_cd = stu_cat_cd;
+	}
+	public String getImg_url()
+	{
+		return img_url;
+	}
+	public void setImg_url(String img_url)
+	{
+		this.img_url = img_url;
+	}
+	
 	public String getStu_cd()
 	{
 		return stu_cd;
